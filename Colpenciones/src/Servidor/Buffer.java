9b4420e7afcd9d4887814ac98655568bac241974
davidmesa <david.mesa.bustamante@gmail.com>
2014-03-02 14:22:32 -0500

    Formato de documentación

--- Colpenciones/src/Servidor/Buffer.java
+++ Colpenciones/src/Servidor/Buffer.java
@@ -1,5 +1,38 @@
 package Servidor;
 
-public class Buffer {
+import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * Clase Buffer
+ * @author davidmesa
+ * Encagada de contener los mensajes listos para que el servidor los consuma
+ */
+public class Buffer {
+	
+	//-----------------------------------------------------------------------
+	//								Atributos
+	//-----------------------------------------------------------------------
+	
+	/**
+	 * Contiene la capacidad del buffer.
+	 */
+	private AtomicInteger capacidad;
+
+	//-----------------------------------------------------------------------
+	//							   Constructor
+	//-----------------------------------------------------------------------
+	
+	/**
+	 * Constructor del Buffer
+	 * @param nCapacidad la capacidad del buffer
+	 */
+	public Buffer ( int nCapacidad )
+	{
+		capacidad = new AtomicInteger(nCapacidad);
+	}
+
+	//-----------------------------------------------------------------------
+	//								Metodos
+	//-----------------------------------------------------------------------
+	
 }
--- Colpenciones/src/Servidor/Servidor.java
+++ Colpenciones/src/Servidor/Servidor.java
@@ -1,5 +1,58 @@
 package Servidor;
 
+/**
+ * Clase servidor
+ * @author davidmesa
+ * Encargada de recibir las peticiones almacenadas en el buffer y retornar
+ * Esta clase incluye el main
+ */
 public class Servidor extends Thread {
+
+	//-----------------------------------------------------------------------
+	//								Atributos
+	//-----------------------------------------------------------------------
 	
+	/**
+	 * 
+	 */
+	private Buffer buffer;
+	
+	//-----------------------------------------------------------------------
+	//							   Constructor
+	//-----------------------------------------------------------------------
+	
+	public Servidor( Buffer nBuffer )
+	{
+		
+	}
+	
+	//-----------------------------------------------------------------------
+	//								  Main
+	//-----------------------------------------------------------------------
+
+	/**
+	 * Main clase, Priera en iniciar
+	 * @param args
+	 */
+	public static void main( String[] args )
+	{
+		Buffer buffer = new Buffer(20);
+
+		// Crea servidores
+
+		int cantServidores = 10;
+
+		Servidor[] servidores = new Servidor[ cantServidores ];
+
+		for (int i = 0; i < cantServidores; i++) 
+		{
+			servidores[i] = new Servidor( buffer );
+			servidores[i].start();
+		}
+
+		//Crea Clientes
+
+		int cantClientes = 30;
+	}
+
 }

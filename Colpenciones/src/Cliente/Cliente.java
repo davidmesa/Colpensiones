817b6b94afe750add9401cb5dbeb842dd943b94c
Miguel Caldas <ma.caldas331@gmail.com>
2014-03-03 14:59:45 -0500

    Clases cliente y mensaje

--- Colpenciones/src/Cliente/Cliente.java
+++ Colpenciones/src/Cliente/Cliente.java
@@ -1,5 +1,63 @@
 package Cliente;
 
-public class Cliente {
+import Servidor.Buffer;
+import Servidor.Servidor;
 
+
+/**
+ * 
+ * @author miguelangelcaldasvillamizar
+ *
+ */
+public class Cliente extends Thread
+{
+
+	
+	private static long cedula;
+	
+	private Buffer buffer;
+	
+	
+	/**
+	 * Metodo constructor de la clase cliente
+	 */
+	public Cliente(long id, Buffer buff)
+	{
+		cedula = id;
+		buffer = buff;
+	}
+	
+	/**
+	 * Metodo que retorna el identificador unico del cliente
+	 * @return un numero que identifica al cliente de todos los demas
+	 */
+	public long darId()
+	{
+		return cedula;
+	}
+	
+	/**
+	 * Metodo del main de los threads
+	 * @param args
+	 */
+	public void main(String[] args)
+	{
+		int cantiClients = 30;
+		Cliente[] clientes = new Cliente[cantiClients];
+		for (int i= 0; i<clientes.length; i++)
+		{
+			clientes[i] = new Cliente(i, buffer);
+			clientes[i].start();
+		}
+	}
+	
+	/**
+	 * Metodo que realiza las operaciones de cada thread
+	 */
+	public void run()
+	{
+		Mensaje mess = new Mensaje(this, buffer);
+		//TODO
+	}
+	
 }
--- Colpenciones/src/Cliente/Mensaje.java
+++ Colpenciones/src/Cliente/Mensaje.java
@@ -1,5 +1,43 @@
 package Cliente;
 
-public class Mensaje {
+import Servidor.Buffer;
 
+/**
+ * 
+ * @author miguelangelcaldasvillamizar
+ *
+ */
+public class Mensaje 
+{
+	private Cliente cliente;
+	
+	private Buffer buffer;
+	
+	/**
+	 * Metodo constructor de la clase
+	 * @param client el cliente que crea el mensaje
+	 */
+	public Mensaje(Cliente client, Buffer buff)
+	{
+		cliente = client;
+		buffer = buff;
+	}
+	
+	/**
+	 * Metodo que retona el cliente que envio este mensaje
+	 * @return el cliente que envia el mensaje
+	 */
+	public Cliente darCliente()
+	{
+		return cliente;
+	}
+	
+	/**
+	 * Metodo que simboliza el envio del mansaje al buffer
+	 */
+	public void envio()
+	{
+		//TODO
+	}
+	
 }
